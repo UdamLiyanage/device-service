772d1104882ea052ed1b3912292ca953ea07a35f
Udam Liyanage <udam1998@gmail.com>
2020-01-04 12:42:35 +0530

    Read operation

--- read_device.go
+++ read_device.go
@@ -1,7 +1,22 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 func readDevice(c *gin.Context) {
-	c.String(200, "Read Device Function")
+	var device Device
+	objID, err := primitive.ObjectIDFromHex(c.Param("id"))
+	if err != nil {
+		panic(err)
+	}
+	filter := bson.M{"_id": objID}
+	err = db.Collection.FindOne(context.TODO(), filter).Decode(&device)
+	if err != nil {
+		panic(err)
+	}
+	c.JSON(200, device)
 }
