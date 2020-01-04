619778dd428137520516b904fcc470aa0681f430
Udam Liyanage <udam1998@gmail.com>
2020-01-04 12:15:11 +0530

    Device model

---
+++ model.go
@@ -0,0 +1,11 @@
+package main
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type Device struct {
+	ID             primitive.ObjectID `json:"_id, omitempty" bson:"_id, omitempty"`
+	Name           string             `json:"name, omitempty" bson:"name, omitempty"`
+	Serial         string             `json:"serial" bson:"serial"`
+	Token          string             `json:"token" bson:"token"`
+	Configurations map[string]string  `json:"configurations, omitempty" bson:"configurations"`
+}
