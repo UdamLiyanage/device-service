12a93f45569d3166d8c6239d734a35b7f01d315c
Udam Liyanage <udam1998@gmail.com>
2020-01-04 12:59:00 +0530

    Model updated to fit configurations array

--- model.go
+++ model.go
@@ -3,8 +3,8 @@ package main
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Device struct {
-	ID             primitive.ObjectID `json:"_id, omitempty" bson:"_id, omitempty"`
-	Name           string             `json:"name, omitempty" bson:"name, omitempty"`
-	Serial         string             `json:"serial" bson:"serial"`
-	Configurations map[string]string  `json:"configurations, omitempty" bson:"configurations"`
+	ID             primitive.ObjectID  `json:"_id, omitempty" bson:"_id, omitempty"`
+	Name           string              `json:"name, omitempty" bson:"name, omitempty"`
+	Serial         string              `json:"serial" bson:"serial"`
+	Configurations []map[string]string `json:"configurations, omitempty" bson:"configurations"`
 }
