365d3ca0f824a10a39481cee51ff419ac9677602
Udam Liyanage <udam1998@gmail.com>
2020-01-09 23:00:49 +0530

    Changed ValidDeviceGet test case

--- read_device_test.go
+++ read_device_test.go
@@ -9,15 +9,11 @@ import (
 func TestValidDeviceGet(t *testing.T) {
 	//Object ID - 5e103f782aa554ae4e6abb8b is always available as a test document
 	r := newRouter()
-	mockServer := httptest.NewServer(r)
-
-	resp, err := http.Get(mockServer.URL + "/devices/5e103f782aa554ae4e6abb8b")
-
-	if err != nil {
-		t.Fatal(err)
-	}
-	if resp.StatusCode != http.StatusOK {
-		t.Errorf("Status should be 200, got %d", resp.StatusCode)
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/devices/5e103f782aa554ae4e6abb8b", nil)
+	r.ServeHTTP(w, req)
+	if w.Code != http.StatusOK {
+		t.Errorf("Status should be 200, got %d", w.Code)
 	}
 }
 
