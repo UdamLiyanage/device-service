808b14ae960f81490b10dd585224aa89765ab9ee
Udam Liyanage <udam1998@gmail.com>
2020-01-09 23:14:45 +0530

    Invalid Update Device Test Case

--- update_device_test.go
+++ update_device_test.go
@@ -24,3 +24,20 @@ func TestUpdateDeviceValid(t *testing.T) {
 		t.Errorf("Status should be 200, got %d", w.Code)
 	}
 }
+
+func TestUpdateDeviceInvalid(t *testing.T) {
+	device := Device{
+		Name: "Updated Name",
+	}
+	body, err := json.Marshal(device)
+	if err != nil {
+		t.Fatal(err)
+	}
+	r := newRouter()
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("PUT", "/devices/5e103f782aa554ae4e6abb8", bytes.NewBuffer(body))
+	r.ServeHTTP(w, req)
+	if w.Code != http.StatusNotFound {
+		t.Errorf("Status should be 404, got %d", w.Code)
+	}
+}
