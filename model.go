9e5a5b0b31d674a9ffeb1c2a010d36dec131ebaa
Udam Liyanage <udam1998@gmail.com>
2020-01-10 22:08:13 +0530

    Fix for SA5008 error

--- model.go
+++ model.go
@@ -4,7 +4,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Device struct {
 	ID             primitive.ObjectID  `json:"_id,omitempty" bson:"_id,omitempty"`
-	Name           string              `json:"name, omitempty" bson:"name, omitempty"`
+	Name           string              `json:"name,omitempty" bson:"name,omitempty"`
 	Serial         string              `json:"serial" bson:"serial"`
-	Configurations []map[string]string `json:"configurations, omitempty" bson:"configurations"`
+	Configurations []map[string]string `json:"configurations,omitempty" bson:"configurations,omitempty"`
 }
